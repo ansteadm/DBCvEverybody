3166f392d15ad08e4160508ae8e91c741921d97e
razgriz-aigbe <37884099+razgriz-aigbe@users.noreply.github.com>
2019-11-24 10:05:37 -0500

    Second commit -no build

--- Liquidid/include/liquidid.hpp
+++ Liquidid/include/liquidid.hpp
@@ -1,59 +1,58 @@
-//#pragma once
-//
-//#include <eosio/eosio.hpp>
-//#include "../dappservices/multi_index.hpp"
-//
-//
-///* add necessary liquidaccount / vram includes */
-//#include "../dappservices/vaccounts.hpp"
-//#include "../dappservices/ipfs.hpp"
-//#include "../dappservices/multi_index.hpp"
-//
-//#define dappservices_actions() \
-//  xsignal_dappservice_action \
-//  ipfs_dappservice_actions
-//#define dappservices_actions_commands() \
-//  ipfs_svc_commands()
-//#define contract_name() liquidid
-//using std::string;
-//
-//using namespace std;
-//using namespace eosio;
-//
-//contract_start()
-//  public:
-//
-//	  liquidid() {}
-//
-//	  [[eosio::action]]
-//	  char generateid();
-//
-//	  [[eosio::action]]
-//	  auto savedata(string data);
-//
-//	  [[eosio::action]]
-//	  auto retrievedata(string id)
-//
-//  private:
-//
-//	  struct[[eosio::table]] shardbucket{
-//		std::vector<char> shard_uri;
-//		uint64_t shard;
-//		uint64_t primary_key() const { return shard }
-//	  }
-//
-//		struct[[eosio::table]] userids{
-//			name vaccount
-//			string  id;
-//			vector<string> userdata;
-//			auto primary_key() const { return id.value; }
-//	  };
-//
-//	  typedef dapp::multi_index<name("userids"), userids> userids_table;
-//
-//	  typedef eosio::multi_index<name(".userids"), userids> userids_table_abi;
-//
-//	  typedef eosio::multi_index<name("userids"), shardbucket> userids_table_abi;
-//
-//
-//	  contract_end((init)(generateid)(savedata)(retrievedata)(xdcommit)()xvinit)
+#pragma once
+
+#include <eosio/eosio.hpp>
+
+
+/* add necessary liquidaccount / vram includes */
+#include "../dappservices/vaccounts.hpp"
+#include "../dappservices/ipfs.hpp"
+#include "../dappservices/multi_index.hpp"
+
+#define dappservices_actions() \
+  xsignal_dappservice_action \
+  ipfs_dappservice_actions
+#define dappservices_actions_commands() \
+  ipfs_svc_commands()
+#define contract_name() liquidid
+using std::string;
+
+using namespace std;
+using namespace eosio;
+
+contract_start()
+  public:
+
+	  liquidid() {}
+
+	  [[eosio::action]]
+	  char generateID();
+
+	  [[eosio::action]]
+	  auto saveData(userids payload string data);
+
+	  [[eosio::action]]
+	  auto retrieveData(string id)
+
+  private:
+
+	  struct[[eosio::table]] shardbucket{
+		std::vector<char> shard_uri;
+		uint64_t shard;
+		uint64_t primary_key() const { return shard }
+	  }
+
+		struct[[eosio::table]] userids{
+			name vaccount
+			string  id;
+			vector<string> userdata;
+			auto primary_key() const { return id.value; }
+	  };
+
+	  typedef dapp::multi_index<name("userids"), userids> userids_table;
+
+	  typedef eosio::multi_index<name(".userids"), userids> userids_table_abi;
+
+	  typedef eosio::multi_index<name("userids"), shardbucket> userids_table_abi;
+
+
+	  contract_end((init)(generateID)(saveData)(retrieveData)(xdcommit)()xvinit)
--- Liquidid/src/liquidid.cpp
+++ Liquidid/src/liquidid.cpp
@@ -1,74 +1,28 @@
-//#include <liquidid.hpp>
-//
-//ACTION liquidid::generateID() {
-//	// Will need to generate cryptographically random key using crypto++
-//
-//	// Set new vaccount
-//	regaccount(userIds)
-//}
-//
-//ACTION liquidid::saveData(userIds payload) {
-//	require_vaccount(payload.vaccount);
-//
-//	messages_table _messages(get_self(), get_self().value);
-//
-//	// Delete all records in _messages table
-//	auto msg_itr = _messages.begin();
-//	while (msg_itr != _messages.end()) {
-//		msg_itr = _messages.erase(msg_itr);
-//	}
-//}
-//
-//EOSIO_DISPATCH(dbctest, (hi)(clear))
+#include <liquidid.hpp>
 
-#define VACCOUNTS_DELAYED_CLEANUP 120
+ACTION liquidid::generateID() {
+	// Will need to generate cryptographically random key using crypto++
 
-#include "../dappservices/vaccounts.hpp"
-#include "../dappservices/ipfs.hpp"
-#include "../dappservices/multi_index.hpp"
+	// Set new vaccount
+	regaccount(userIds)
+}
 
-#define DAPPSERVICES_ACTIONS() \
-  XSIGNAL_DAPPSERVICE_ACTION \
-  IPFS_DAPPSERVICE_ACTIONS \
-  VACCOUNTS_DAPPSERVICE_ACTIONS
-#define DAPPSERVICE_ACTIONS_COMMANDS() \
-  IPFS_SVC_COMMANDS()VACCOUNTS_SVC_COMMANDS() 
-
-#define CONTRACT_NAME() vaccountsconsumer 
-
-
-CONTRACT_START()
-
-
-struct dummy_action_hello {
-	name vaccount;
-	uint64_t b;
-	uint64_t c;
-
-	EOSLIB_SERIALIZE(dummy_action_hello, (vaccount)(b)(c))
-};
-
-[[eosio::action]] void hello(dummy_action_hello payload) {
+ACTION liquidid::saveData(userids payload, string data) {
 	require_vaccount(payload.vaccount);
 
-	print("hello from ");
-	print(payload.vaccount);
-	print(" ");
-	print(payload.b + payload.c);
-	print("\n");
+	// Push data into payload's vector
+	payload.userdata.push_back(data);
+
+	return payload
 }
 
-[[eosio::action]] void hello2(dummy_action_hello payload) {
-	print("hello2(default action) from ");
-	print(payload.vaccount);
-	print(" ");
-	print(payload.b + payload.c);
-	print("\n");
+ACTION liquidid::retrieveData(userids payload) {
+	require_vaccount(payload.vaccount);
+
+	// Get account with vector data
+	getKey(payload.vaccount)
+
+	return payload
 }
 
-[[eosio::action]] void init(dummy_action_hello payload) {
-}
-
-VACCOUNTS_APPLY(((dummy_action_hello)(hello))((dummy_action_hello)(hello2)))
-
-CONTRACT_END((init)(hello)(hello2)(regaccount)(xdcommit)(xvinit))
+EOSIO_DISPATCH(liquidid, (generateID)(saveData)(retrieveData))
